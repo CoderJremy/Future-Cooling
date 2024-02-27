044225042c8bc284e6683ef213ddce10ac4e06b2
CoderJremy <jeremy66frias@gmail.com>
2024-02-27 06:35:29 -0500

    Created Info Section

---
+++ src/Info.jsx
@@ -0,0 +1,16 @@
+const Info = () => {
+    return (
+        <div id="info">
+            <div className="part">
+                <img src="" alt="" />
+                <p>Text from the top.</p>
+            </div>
+            <div className="part">
+                <img src="" alt="" />
+                <p>Text from the bottom.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
